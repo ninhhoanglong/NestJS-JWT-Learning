c2d26069c00058d845b3db292fe337a87ceac5ba
ninhhoanglong <84828423+ninhhoanglong@users.noreply.github.com>
2023-08-10 14:55:14 +0700

    Register with hashed password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDTO } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('register')
-  register() {
-    return this.authService.register();
+  register(@Body() authDTO: AuthDTO) {
+    console.log(authDTO);
+    return this.authService.register(authDTO);
   }
 
   @Post('login')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDTO } from './dto';
+import * as argon from 'argon2';
 @Injectable({})
 export class AuthService {
-  register() {
-    return {
-      message: 'Register an user',
-    };
+  constructor(private prismaService: PrismaService) {}
+  async register(authDTO: AuthDTO) {
+    const hashedPassword = await argon.hash(authDTO.password);
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: authDTO.email,
+          hashedPassword,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error.code == 'P2002') {
+        throw new ForbiddenException('Error in credentials');
+      }
+    }
   }
   login() {
     return {
